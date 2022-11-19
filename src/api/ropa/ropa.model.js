74e0984c9e5f439634e3c3c769ba64a795cdc72c
clauadri <adripa1111@gmail.com>
2022-11-19 17:17:29 +0100

    change model

--- src/api/ropa/ropa.model.js
+++ src/api/ropa/ropa.model.js
@@ -5,7 +5,7 @@ const ropaSchema = new mongoose.Schema(
     tipo: { type: String, trim: true, required: true },
     precio: { type: Number, trim: true, required: true },
     imagen: { type: String, trim: true, required: true },
-    talla: { type: String, trim: true,enum:['XS','S','M','L','XL','XXL'], required: true }
+    talla: { type: String, trim: true,enum:['XS','S','M','L','XL','XXL'] }
   },
   { timestamps: true, collection: "ropas" }
 );
